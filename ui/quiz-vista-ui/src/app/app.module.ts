8d8ca501e6cb5dc51207ae310e3f7a606f73a0c6
Michał Robakowski <48829502+michaus736@users.noreply.github.com>
2023-10-29 20:27:27 +0100

    init angular app

---
+++ ui/quiz-vista-ui/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminPanelComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
