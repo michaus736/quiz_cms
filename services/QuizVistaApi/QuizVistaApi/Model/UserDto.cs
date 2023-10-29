b519d3f04f0529a585129f6a556b5fc9c38ce177
xkrlx <karol1999kar@gmail.com>
2023-10-29 22:38:55 +0100

    Adding JWT

---
+++ services/QuizVistaApi/QuizVistaApi/Model/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizVistaApi.Model
+{
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
