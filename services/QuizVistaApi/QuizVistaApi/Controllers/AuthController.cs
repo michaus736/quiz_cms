3fea1f272531d44f35c9ba4767db967b04e0a7c5
xkrlx <karol1999kar@gmail.com>
2023-10-30 17:42:25 +0100

    Role-Based Authorization

--- services/QuizVistaApi/QuizVistaApi/Controllers/AuthController.cs
+++ services/QuizVistaApi/QuizVistaApi/Controllers/AuthController.cs
@@ -52,7 +52,8 @@ namespace QuizVistaApi.Controllers
         {
             List<Claim> claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name,user.Username)
+                new Claim(ClaimTypes.Name,user.Username),
+                new Claim(ClaimTypes.Role,"Admin")
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
--- services/QuizVistaApi/QuizVistaApi/obj/Debug/net7.0/QuizVistaApi.AssemblyInfo.cs
+++ services/QuizVistaApi/QuizVistaApi/obj/Debug/net7.0/QuizVistaApi.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("248cca73-d5cd-4c33-8235-5424d40068b9")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizVistaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
